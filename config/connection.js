d130e1d2821162c41ae3a0c14f7bc4c8d90b70d4
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-03-09 21:11:12 -0500

    connection link complete

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(DB_NAME, DB_USER, DB_PW, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
--- models/Product.js
+++ models/Product.js
@@ -20,7 +20,7 @@ Product.init(
       allowNull: false
     },
     price: {
-      type: DatTypes.DECIMAL,
+      type: DataTypes.DECIMAL,
       allowNull: false,
       validate:{
         isDecimal: true
