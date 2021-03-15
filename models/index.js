db4f419950e3b5589c4e3bb473b490236916c859
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-03-15 10:43:30 -0400

    queries updated and errors fixed

--- models/index.js
+++ models/index.js
@@ -20,14 +20,14 @@ Category.hasMany(Product, {
 Product.belongsToMany(Tag, {
   through: ProductTag,
   //as: 'product_tag',
-  foreignKey: 'tag_id'  
+  foreignKey: 'product_id'  
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   //as: 'product_tag',
-  foreignKey: 'product_id'
+  foreignKey: 'tag_id'
 });
 
 module.exports = {
